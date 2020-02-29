597245f9d0566721e23c45d0ad11de5315e0516c
Sean Dez <sd@seandezoysa.com>
2020-03-01 03:23:28 +0700

    Updates

---
+++ precache-manifest.c97742fbfe6f82012b92c728bb229ae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a06d427e1ff68e43e62813525e73f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "273be6fedbaec0cff014",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d738792368c691805ea7",
+    "url": "/static/js/2.d9a66d93.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d9a66d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273be6fedbaec0cff014",
+    "url": "/static/js/main.8100467e.chunk.js"
+  },
+  {
+    "revision": "683718c4996d47f0065f",
+    "url": "/static/js/runtime-main.c92831a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
